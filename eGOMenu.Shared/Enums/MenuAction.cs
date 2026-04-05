c015bbfa8752c30e5c1eafb6053ad271f786e0a3
ShookEagle <shookeagle@gmail.com>
2026-04-05 14:51:42 -0500

    Add MenuAction enum to define menu action types

---
+++ eGOMenu.Shared/Enums/MenuAction.cs
@@ -0,0 +1,24 @@
+﻿namespace eGOMenu.Shared.Enums;
+
+public enum MenuAction {
+  /// <summary>Begins displaying the menu for the first time.</summary>
+  START,
+
+  /// <summary>Invoked when a selectable item is activated.</summary>
+  SELECT,
+
+  /// <summary>Invoked when a choice among many options is made.</summary>
+  CHOOSE,
+
+  /// <summary>Invoked when a menu element updates a value.</summary>
+  UPDATE,
+
+  /// <summary>Invoked when the user exits the menu.</summary>
+  EXIT,
+
+  /// <summary>Additional helper action (reserved for future use).</summary>
+  ASSIST,
+
+  /// <summary>Invoked when textual input is submitted.</summary>
+  INPUT
+}
